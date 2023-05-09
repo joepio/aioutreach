666b092cbf0abf73e43e073b466b0d2c76d9dfad
FabFabtastic <127984058+FabFabtastic@users.noreply.github.com>
2023-05-09 21:52:15 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Rubik:wght@300&display=swap" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&family=Montserrat:wght@500&family=Rubik:wght@300&display=swap" rel="stylesheet"> 
 </svelte:head>
 
 <div class="layout">
